23a0b885c5602f6f92bf3c7a156231469e3339ac
carmenlancho <carmenlancho@hotmail.com>
2023-04-13 11:22:08 +0200

    First results with pyhard

---
+++ ISA_loading.py
@@ -0,0 +1,9 @@
+#### How to run
+# In terminal: pyhard run # to obtain all results, then metadata_full.csv contains all the complexity measures
+# In terminal: pyhard run --no-isa # to obtain complexity measures results without model information,
+# then metadata.csv is the file of interest
+
+
+### DOUBTS:
+## How to automatically execute the code for several datasets?
+## How to select the path where the results are saved?
