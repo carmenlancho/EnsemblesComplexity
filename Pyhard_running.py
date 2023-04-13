5019380b33b0c28b9a05c137d53c0fc722c362de
carmenlancho <carmenlancho@hotmail.com>
2023-04-13 11:25:44 +0200

    Comments about guidelines for input dataset

--- Pyhard_running.py
+++ Pyhard_running.py
@@ -7,3 +7,13 @@
 ### DOUBTS:
 ## How to automatically execute the code for several datasets?
 ## How to select the path where the results are saved?
+
+
+##### Guidelines for input dataset
+## Please follow the recommendations below:
+# Only csv files are accepted
+# The dataset should be in the format (n_instances, n_features)
+# It cannot contains NaNs or missing values
+# Do not include any index column. Instances will be indexed in order, starting from 1
+# The last column should contain the target variable (y). Otherwise, the name of the target column must be declared in the field target_col (config file)
+# Categorical features should be handled previously
