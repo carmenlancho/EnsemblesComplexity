661fc92d5c78e972c521f32f1996aa75a388c53e
carmenlancho <carmenlancho@hotmail.com>
2023-04-17 13:40:36 +0200

    3 more artificial datasets

--- ArtificialData_makeclassification.py
+++ ArtificialData_makeclassification.py
@@ -1,12 +1,13 @@
 # Generation of artificial data with make_classification
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 import os
 root_path = os.getcwd()
 
 X, y = make_classification(
-    n_samples=3000, # 3000 observations
+    n_samples=500, # 3000 observations
     n_features=2, # 2 total features
     n_informative=2,n_redundant=0,
     n_classes=2, # binary target/label
@@ -14,9 +15,9 @@ X, y = make_classification(
     hypercube = True,
     shift = 0.0, # just changes the scale
     shuffle = True, # Shuffle the samples and the features
-    class_sep = 0.6,
-    flip_y=0.0,
-    random_state=28 #
+    class_sep = 0.2,
+    flip_y=0.4,
+    random_state=14 #
 )
 
 data = pd.DataFrame(X, columns=['x1','x2'])
@@ -99,6 +100,51 @@ data.to_csv('data4_makeclass.csv', index=False)
 #     random_state=28 #
 # )
 
+data.to_csv('Dataset14.csv', index=False)
+# X, y = make_classification(
+#     n_samples=1000, # 3000 observations
+#     n_features=2, # 2 total features
+#     n_informative=2,n_redundant=0,
+#     n_classes=2, # binary target/label
+#     n_clusters_per_class = 1,
+#     hypercube = True,
+#     shift = 0.0, # just changes the scale
+#     shuffle = True, # Shuffle the samples and the features
+#     class_sep = 0.5,
+#     flip_y=0.4,
+#     random_state=3 #
+# )
+
+
+data.to_csv('Dataset15.csv', index=False)
+# X, y = make_classification(
+#     n_samples=2000, # 3000 observations
+#     n_features=2, # 2 total features
+#     n_informative=2,n_redundant=0,
+#     n_classes=2, # binary target/label
+#     n_clusters_per_class = 1,
+#     hypercube = True,
+#     shift = 0.0, # just changes the scale
+#     shuffle = True, # Shuffle the samples and the features
+#     class_sep = 0.3,
+#     flip_y=0.4,
+#     random_state=6 #
+# )
+
+data.to_csv('Dataset16.csv', index=False)
+# X, y = make_classification(
+#     n_samples=500, # 3000 observations
+#     n_features=2, # 2 total features
+#     n_informative=2,n_redundant=0,
+#     n_classes=2, # binary target/label
+#     n_clusters_per_class = 2,
+#     hypercube = True,
+#     shift = 0.0, # just changes the scale
+#     shuffle = True, # Shuffle the samples and the features
+#     class_sep = 0.2,
+#     flip_y=0.4,
+#     random_state=14 #
+# )
 
 from sklearn.datasets import make_blobs, make_moons, make_checkerboard
 
