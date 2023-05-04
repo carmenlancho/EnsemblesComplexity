247fbe59760286a5c7d82ee2e41762bbc4f8acd6
carmenlancho <carmenlancho@hotmail.com>
2023-05-04 21:08:20 +0200

    results aggregation

--- ComplexityDriven_Bagging.py
+++ ComplexityDriven_Bagging.py
@@ -125,8 +125,8 @@ def complexity_driven_bagging(X,y,n_ensembles, name_data,path_to_save):
                 weights = np.repeat(1/len(y_train), len(y_train), axis=0)
             else: # Sampling using Complexity measures
                 CM_values = df_measures[CM_selected]
-                # ranking = CM_values.rank(method='max', ascending=True) # more weight to difficult
-                ranking = CM_values.rank(method='max', ascending=False)  # more weight to easy
+                ranking = CM_values.rank(method='max', ascending=True) # more weight to difficult
+                # ranking = CM_values.rank(method='max', ascending=False)  # more weight to easy
                 weights = ranking/sum(ranking) # probability distribution
 
 
@@ -231,9 +231,105 @@ def complexity_driven_bagging(X,y,n_ensembles, name_data,path_to_save):
     results.to_csv(nombre_csv, encoding='utf_8_sig',index=False)
 
 
+    ##### Agregation of results
+    cols_numeric = results.select_dtypes([np.number]).columns[1:]
+    df_aggre_mean = results.groupby(['n_ensemble', 'weights'], as_index=False)[cols_numeric].mean()
+    df_aggre_mean.columns = ['n_ensemble', 'weights', 'accuracy_mean',
+                             'Boots_Hostility_dataset_mean', 'Boots_kDN_dataset_mean', 'Boots_DCP_dataset_mean',
+                             'Boots_TD_U_dataset_mean', 'Boots_CLD_dataset_mean', 'Boots_N1_dataset_mean',
+                             'Boots_N2_dataset_mean', 'Boots_LSC_dataset_mean', 'Boots_F1_dataset_mean']
+    df_aggre_std = results.groupby(['n_ensemble', 'weights'], as_index=False)[cols_numeric].std()
+    df_aggre_std.columns = ['n_ensemble', 'weights', 'accuracy_std', 'Boots_Hostility_dataset_std',
+                            'Boots_kDN_dataset_std', 'Boots_DCP_dataset_std',
+                            'Boots_TD_U_dataset_std', 'Boots_CLD_dataset_std', 'Boots_N1_dataset',
+                            'Boots_N2_dataset_std', 'Boots_LSC_dataset_std', 'Boots_F1_dataset_std']
+
+    df_aggre = pd.concat([df_aggre_mean, df_aggre_std.iloc[:, 2:]], axis=1)
+
+    n_df = df_aggre.shape[0]
+    cols_names = ['confusion_matrix',
+                  'Boots_Hostility_class_mean', 'Boots_kDN_class_mean', 'Boots_DCP_class_mean',
+                  'Boots_TD_U_class_mean', 'Boots_CLD_class_mean', 'Boots_N1_class_mean',
+                  'Boots_N2_class_mean', 'Boots_LSC_class_mean', 'Boots_F1_class_mean',
+                  'Boots_Hostility_class_std', 'Boots_kDN_class_std', 'Boots_DCP_class_std',
+                  'Boots_TD_U_class_std', 'Boots_CLD_class_std', 'Boots_N1_class',
+                  'Boots_N2_class_std', 'Boots_LSC_class_std', 'Boots_F1_class_std']
+    df_lists = pd.DataFrame(0, index=np.arange(n_df), columns=cols_names)
+    df_aggre = pd.concat([df_aggre, df_lists], axis=1)
+
+    n_ensemble_list = np.unique(results['n_ensemble']).tolist()
+    weights_list = np.unique(results['weights']).tolist()
+
+    for n_i in n_ensemble_list:
+        print(n_i)
+        for w in weights_list:
+            print(w)
+            condition = (results.n_ensemble == n_i) & (results.weights == w)
+            data_pack = results.loc[condition]
+            conf_list = np.array(data_pack['confusion_matrix'].tolist())  # format
+            conf_fold = np.sum(conf_list, axis=0).tolist()
+            df_aggre.loc[condition, 'confusion_matrix'] = str(conf_fold)
+
+            Host_class_list = np.array(data_pack['Boots_Hostility_class'].tolist())
+            Host_class_fold_mean = np.mean(Host_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_Hostility_class_mean'] = str(Host_class_fold_mean)
+            Host_class_fold_std = np.std(Host_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_Hostility_class_std'] = str(Host_class_fold_std)
+
+            kdn_class_list = np.array(data_pack['Boots_kDN_class'].tolist())
+            kdn_class_fold_mean = np.mean(kdn_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_kDN_class_mean'] = str(kdn_class_fold_mean)
+            kdn_class_fold_std = np.std(kdn_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_kDN_class_std'] = str(kdn_class_fold_std)
+
+            dcp_class_list = np.array(data_pack['Boots_DCP_class'].tolist())
+            dcp_class_fold_mean = np.mean(dcp_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_DCP_class_mean'] = str(dcp_class_fold_mean)
+            dcp_class_fold_std = np.std(dcp_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_DCP_class_std'] = str(dcp_class_fold_std)
+
+            tdu_class_list = np.array(data_pack['Boots_TD_U_class'].tolist())
+            tdu_class_fold_mean = np.mean(tdu_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_TD_U_class_mean'] = str(tdu_class_fold_mean)
+            tdu_class_fold_std = np.std(tdu_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_TD_U_class_std'] = str(tdu_class_fold_std)
+
+            cld_class_list = np.array(data_pack['Boots_CLD_class'].tolist())
+            cld_class_fold_mean = np.mean(cld_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_CLD_class_mean'] = str(cld_class_fold_mean)
+            cld_class_fold_std = np.std(cld_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_CLD_class_std'] = str(cld_class_fold_std)
+
+            n1_class_list = np.array(data_pack['Boots_N1_class'].tolist())
+            n1_class_fold_mean = np.mean(n1_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_N1_class_mean'] = str(n1_class_fold_mean)
+            n1_class_fold_std = np.std(n1_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_N1_class_std'] = str(n1_class_fold_std)
+
+            n2_class_list = np.array(data_pack['Boots_N2_class'].tolist())
+            n2_class_fold_mean = np.mean(n2_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_N2_class_mean'] = str(n2_class_fold_mean)
+            n2_class_fold_std = np.std(n2_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_N2_class_std'] = str(n2_class_fold_std)
+
+            lsc_class_list = np.array(data_pack['Boots_LSC_class'].tolist())
+            lsc_class_fold_mean = np.mean(lsc_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_LSC_class_mean'] = str(lsc_class_fold_mean)
+            lsc_class_fold_std = np.std(lsc_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_LSC_class_std'] = str(lsc_class_fold_std)
+
+            f1_class_list = np.array(data_pack['Boots_F1_class'].tolist())
+            f1_class_fold_mean = np.mean(f1_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_F1_class_mean'] = str(f1_class_fold_mean)
+            f1_class_fold_std = np.std(f1_class_list, axis=0).tolist()
+            df_aggre.loc[condition, 'Boots_F1_class_std'] = str(f1_class_fold_std)
+
+    # To save the results
+    os.chdir(path_to_save)
+    nombre_csv_agg = 'AggregatedResults_Bagging_' + name_data + '_MoreWeightDifficultInstances.csv'
+    df_aggre.to_csv(nombre_csv_agg, encoding='utf_8_sig',index=False)
+
     return results
-
-
 
 
 path_csv = os.chdir(root_path+'/datasets')
