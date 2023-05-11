f189100e2f53473100a5bae92089538c960c5cf0
carmenlancho <carmenlancho@hotmail.com>
2023-05-11 19:44:44 +0200

    combo split gradually

--- ComplexityDriven_Bagging.py
+++ ComplexityDriven_Bagging.py
@@ -576,6 +576,184 @@ def complexity_driven_bagging_combo(X,y,n_ensembles, name_data,path_to_save, emp
 
 
 
+def complexity_driven_bagging_combo_split(X,y,n_ensembles, name_data,path_to_save, emphasis, split):
+
+    # dataframe to save the results
+    results = pd.DataFrame(columns=['dataset','fold','n_ensemble','weights','confusion_matrix','accuracy',
+                                    'Boots_Hostility_dataset','Boots_kDN_dataset','Boots_DCP_dataset',
+                                    'Boots_TD_U_dataset','Boots_CLD_dataset', 'Boots_N1_dataset',
+                                    'Boots_N2_dataset','Boots_LSC_dataset','Boots_F1_dataset',
+                                    'Boots_Hostility_class', 'Boots_kDN_class','Boots_DCP_class',
+                                    'Boots_TD_U_class','Boots_CLD_class','Boots_N1_class',
+                                    'Boots_N2_class','Boots_LSC_class','Boots_F1_class'])
+
+    # Complexity measures list to check
+    CM_list = ['Hostility', 'kDN', 'DCP','TD_U', 'CLD', 'N1', 'N2','LSC','F1','Uniform']
+    # CM_selected = 'Hostility'
+
+    skf = StratifiedKFold(n_splits=5, random_state=1,shuffle=True)
+    fold = 0
+    for train_index, test_index in skf.split(X, y):
+        fold = fold + 1
+        print(fold)
+        # print(train_index)
+        # print(test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        # print(X_test)
+        # print(y_test)
+
+        # Obtain complexity measures on train set
+        data_train = pd.DataFrame(X_train, columns=['x1','x2'])
+        data_train['y'] = y_train
+        df_measures, _ = all_measures(data_train,False,None, None)
+        # Selection of complexity measures
+        df_measures_sel = df_measures[['Hostility', 'kDN', 'DCP','TD_U', 'CLD', 'N1', 'N2','LSC','F1','y']]
+
+        for CM_selected in CM_list:
+            print(CM_selected)
+
+            if (CM_selected == 'Uniform'): # classic Bagging with uniform probability sampling
+                weights = np.repeat(1/len(y_train), len(y_train), axis=0)
+            else: # Sampling using Complexity measures
+                CM_values = df_measures[CM_selected]
+
+                if (emphasis == 'combo_split'):
+                    ranking_easy = CM_values.rank(method='max', ascending=False)  # more weight to easy
+                    ranking_hard = CM_values.rank(method='max', ascending=True)  # more weight to difficult
+                    weights_easy = ranking_easy / sum(ranking_easy)  # probability distribution
+                    weights_hard = ranking_hard / sum(ranking_hard)  # probability distribution
+                    w_frac = (weights_hard - weights_easy) / split
+                    weights_v = pd.DataFrame()
+                    for s in range(split+1):
+                        print(s)
+                        new_w = weights_easy + s*w_frac
+                        weights_v = pd.concat([weights_v,new_w],axis=1)
+
+
+
+
+
+            preds = pd.DataFrame()
+            ensemble_preds = pd.DataFrame()
+            # i = 0
+            j = 0
+            for i in range(n_ensembles):
+
+                # print(i)
+                # Get bootstrap sample following CM_weights
+                n_train = len(y_train)
+
+                if (CM_selected == 'Uniform'):
+                    np.random.seed(1)
+                    X_bootstrap, y_bootstrap, bootstrap_indices = bootstrap_sample(X_train, y_train, weights)
+                else:
+                    n_ensembles / (split + 1)
+                    split
+                    index_w = 6 / (split + 1)
+                    if (j <= split):
+                        weights = weights_v.iloc[:,i]
+                    else:
+                        j = 0
+                        weights = weights_v.iloc[:, i]
+                    j = j + 1
+
+                    np.random.seed(1)
+                    X_bootstrap, y_bootstrap, bootstrap_indices = bootstrap_sample(X_train, y_train, weights)
+
+
+                # Save complexity information (class and dataset levels)
+                df_measures_bootstrap = df_measures_sel.iloc[bootstrap_indices].copy()
+                # Binarize hostility to obtain class and dataset levels
+                df_measures_bootstrap.loc[:, 'Hostility_bin'] = np.where(df_measures_bootstrap['Hostility'] >= 0.5, 1,0)
+
+                # Dataset
+                [Boots_kDN_dataset,Boots_DCP_dataset,Boots_TD_U_dataset,
+                 Boots_CLD_dataset,Boots_N1_dataset,Boots_N2_dataset,
+                 Boots_LSC_dataset,Boots_F1_dataset] = df_measures_bootstrap.mean()[1:-2]
+                Boots_Hostility_dataset = df_measures_bootstrap.mean()['Hostility_bin']
+                # Class
+                df_classes_boots = df_measures_bootstrap.groupby('y').mean()
+                Boots_Hostility_class = df_classes_boots['Hostility_bin'].tolist()
+                Boots_kDN_class = df_classes_boots['kDN'].tolist()
+                Boots_DCP_class = df_classes_boots['DCP'].tolist()
+                Boots_TD_U_class = df_classes_boots['TD_U'].tolist()
+                Boots_CLD_class = df_classes_boots['CLD'].tolist()
+                Boots_LSC_class = df_classes_boots['LSC'].tolist()
+                Boots_N1_class = df_classes_boots['N1'].tolist()
+                Boots_N2_class = df_classes_boots['N2'].tolist()
+                Boots_F1_class = df_classes_boots['F1'].tolist()
+
+
+                # Train DT in bootstrap sample and test y X_test, y_test
+                clf = DecisionTreeClassifier(random_state=0)
+                clf.fit(X_bootstrap, y_bootstrap)
+                y_pred = clf.predict(X_test)
+
+                if (i==0): # first iteration
+                    col_name = 'pred_' + str(i)
+                    preds[col_name] = y_pred  # individual predictions
+                    # ensemble_preds = pd.DataFrame(y_pred, columns=['pred_0'])
+                    y_predicted = y_pred
+                else:
+                    col_name = 'pred_'+str(i)
+                    preds[col_name] = y_pred # individual predictions
+                    votes = voting_rule(preds)
+                    # print(votes)
+                    votes_dict = {'col_name':votes}
+                    votes_df = pd.DataFrame(votes_dict)
+                    votes_df.columns = [col_name]
+                    # ensemble_preds[col_name] = votes # ensemble prediction with majority voting rule
+                    ensemble_preds = pd.concat([ensemble_preds, votes_df], axis=1)
+                    # print(ensemble_preds[col_name])
+
+                    y_predicted = ensemble_preds.iloc[:, -1:] # last column
+                acc = accuracy_score(y_predicted, y_test)
+                conf_matrix = confusion_matrix(y_test, y_predicted).tolist()
+
+                results_dict = {'dataset':name_data,'fold':fold, 'n_ensemble':i, 'weights':CM_selected,
+                                    'confusion_matrix':[conf_matrix], 'accuracy':acc,
+                                    'Boots_Hostility_dataset':Boots_Hostility_dataset,
+                                    'Boots_kDN_dataset': Boots_kDN_dataset,
+                                    'Boots_DCP_dataset': Boots_DCP_dataset,
+                                    'Boots_TD_U_dataset': Boots_TD_U_dataset,
+                                    'Boots_CLD_dataset': Boots_CLD_dataset,
+                                    'Boots_N1_dataset': Boots_N1_dataset,
+                                    'Boots_N2_dataset': Boots_N2_dataset,
+                                    'Boots_LSC_dataset': Boots_LSC_dataset,
+                                    'Boots_F1_dataset': Boots_F1_dataset,
+
+                                    'Boots_Hostility_class': [Boots_Hostility_class],
+                                    'Boots_kDN_class': [Boots_kDN_class],
+                                    'Boots_DCP_class': [Boots_DCP_class],
+                                    'Boots_TD_U_class': [Boots_TD_U_class],
+                                    'Boots_CLD_class': [Boots_CLD_class],
+                                    'Boots_N1_class': [Boots_N1_class],
+                                    'Boots_N2_class': [Boots_N2_class],
+                                    'Boots_LSC_class': [Boots_LSC_class],
+                                    'Boots_F1_class': [Boots_F1_class]}
+                results_aux = pd.DataFrame(results_dict, index=[0])
+                results = pd.concat([results,results_aux])
+
+    # To save the results
+    os.chdir(path_to_save)
+    nombre_csv = 'Bagging_' + name_data + '_MoreWeight_' + emphasis +'_Instances.csv'
+    results.to_csv(nombre_csv, encoding='utf_8_sig',index=False)
+
+    ##### Agregation of results
+    df_aggre = aggregation_results(results)
+
+    # To save the results
+    os.chdir(path_to_save)
+    nombre_csv_agg = 'AggregatedResults_Bagging_' + name_data + '_MoreWeight_' + emphasis + '_split' + str(split)+ '_Instances.csv'
+    df_aggre.to_csv(nombre_csv_agg, encoding='utf_8_sig',index=False)
+
+    return results
+
+
+
+
+
 path_csv = os.chdir(root_path+'/datasets')
 # Extraemos los nombres de todos los ficheros
 total_name_list = []
@@ -599,10 +777,13 @@ for data_file in total_name_list:
     X = data[['x1', 'x2']].to_numpy()
     X = preprocessing.scale(X)
     y = data[['y']].to_numpy()
-    emphasis_combo = 'combo'
-    results = complexity_driven_bagging_combo(X, y, n_ensembles, name_data, path_to_save,emphasis_combo)
-    emphasis_combo_classes = 'combo_classes'
-    results2 = complexity_driven_bagging_combo(X, y, n_ensembles, name_data, path_to_save,emphasis_combo_classes)
+    split = 2
+    emphasis = 'combo_split'
+    results = complexity_driven_bagging_combo_split(X,y,n_ensembles, name_data,path_to_save, emphasis, split)
+    split4 = 4
+    results2 = complexity_driven_bagging_combo_split(X,y,n_ensembles, name_data,path_to_save, emphasis, split4)
+    split9 = 9
+    results3 = complexity_driven_bagging_combo_split(X,y,n_ensembles, name_data,path_to_save, emphasis, split9)
 
 
 
