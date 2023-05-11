1af47e5600e0ff6e3ed5da6b6f647ecf2b30ab5d
carmenlancho <carmenlancho@hotmail.com>
2023-05-11 17:22:35 +0200

    analysis and comparison of easy-hard-combo

--- Analysis_results.py
+++ Analysis_results.py
@@ -59,6 +59,38 @@ def plot_acc_ensemble_averaged(data,name):
 
     return
 
+
+
+def plot_acc_ensemble_and_average(data,data2,name):
+    plt.plot(data.loc[data.weights == 'CLD','n_ensemble'],
+             data.loc[data.weights == 'CLD','accuracy_mean'], c = 'blue', label = 'CLD', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'DCP','n_ensemble'],
+             data.loc[data.weights == 'DCP','accuracy_mean'], c = 'green', label = 'DCP', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'LSC','n_ensemble'],
+             data.loc[data.weights == 'LSC','accuracy_mean'], c = 'lime', label = 'LSC', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'TD_U','n_ensemble'],
+             data.loc[data.weights == 'TD_U','accuracy_mean'], c = 'orange', label = 'TD_U', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'N2','n_ensemble'],
+             data.loc[data.weights == 'N2','accuracy_mean'], c = 'purple', label = 'N2', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'F1','n_ensemble'],
+             data.loc[data.weights == 'F1','accuracy_mean'], c = 'cyan', label = 'F1', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'Uniform','n_ensemble'],
+             data.loc[data.weights == 'Uniform','accuracy_mean'], c = 'k', label = 'Uniform')
+    plt.plot(data.loc[data.weights == 'N1','n_ensemble'],
+             data.loc[data.weights == 'N1','accuracy_mean'], c = 'pink', label = 'N1', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'kDN','n_ensemble'],
+             data.loc[data.weights == 'kDN','accuracy_mean'], c = 'gold', label = 'kDN', alpha = 0.5)
+    plt.plot(data.loc[data.weights == 'Hostility','n_ensemble'],
+             data.loc[data.weights == 'Hostility','accuracy_mean'], c = 'crimson', label = 'Hostility', alpha = 0.5)
+    plt.plot(data2.loc[data2.weights == 'Averaged_measures','n_ensemble'],
+             data2.loc[data2.weights == 'Averaged_measures','accuracy_mean'], c = 'red', label = 'Averaged_measures')
+    plt.legend(loc='lower center', bbox_to_anchor=(0.55, 0.0),
+          ncol=4, fancybox=False, shadow=False)
+    plt.title(name)
+    plt.show()
+
+    return
+
 #######################################################################
 #################    More weight in hard instances WITH RANKING   #################
 #######################################################################
@@ -66,17 +98,49 @@ path_csv = os.chdir(root_path+'/Bagging_results')
 # Extraemos los nombres de todos los ficheros
 total_name_list = []
 for filename in os.listdir(path_csv):
-    if (filename.endswith('.csv') and 'hard' in filename and 'Aggregated' in filename and '1n' not in filename and 'classes' not in filename):
+    if (filename.endswith('.csv') and 'hard' in filename and 'Aggregated' in filename and '1n' not in filename and 'average' not in filename and 'classes' not in filename):
         total_name_list.append(filename)
+total_name_list.sort()
+
+total_name_list2 = []
+for filename in os.listdir(path_csv):
+    if (filename.endswith('.csv') and 'hard' in filename and 'Aggregated' in filename and '1n' not in filename and 'average' in filename and 'classes' not in filename):
+        total_name_list2.append(filename)
+total_name_list2.sort()
 
 
+
+# for file in total_name_list:
+#     os.chdir(root_path + '/Bagging_results')
+#     print(file)
+#     name = file[25:32]
+#     data = pd.read_csv(file)
+#     plot_acc_ensemble(data, name)
+
+for i in range(len(total_name_list)):
+    print(i)
+    os.chdir(root_path + '/Bagging_results')
+    file1 = total_name_list[i]
+    file2 = total_name_list2[i]
+    name = file1[25:32]
+    data = pd.read_csv(file1)
+    data2 = pd.read_csv(file2)
+    plot_acc_ensemble_and_average(data,data2, name)
+
+
+path_to_save = root_path+'/Analysis_results'
 for file in total_name_list:
     os.chdir(root_path + '/Bagging_results')
     print(file)
-    name = file[25:32]
+    name = file[25:]
     data = pd.read_csv(file)
-    plot_acc_ensemble(data, name)
 
+    data_n = data[data['n_ensemble'].isin([15,50,100,150,199])]
+    res = data_n[['n_ensemble','weights','accuracy_mean','accuracy_std']].sort_values(by = ['weights', 'n_ensemble'])#.T
+    # To save the results
+    os.chdir(path_to_save)
+    nombre_csv = 'ResAccuracy_Bagging' + name
+    res.to_csv(nombre_csv, encoding='utf_8_sig',index=True)
 
 
 #######################################################################
@@ -86,7 +150,7 @@ path_csv = os.chdir(root_path+'/Bagging_results')
 # Extraemos los nombres de todos los ficheros
 total_name_list = []
 for filename in os.listdir(path_csv):
-    if (filename.endswith('.csv') and 'easy' in filename and 'Aggregated' in filename and '1n' not in filename and 'classes' not in filename):
+    if (filename.endswith('.csv') and 'easy' in filename and 'Aggregated' in filename and '1n' not in filename and 'classes' not in filename and 'averaged' not in filename):
         total_name_list.append(filename)
 
 
@@ -106,7 +170,7 @@ for file in total_name_list:
     data = pd.read_csv(file)
 
     data_n = data[data['n_ensemble'].isin([15,50,100,150,199])]
-    res = data_n[['n_ensemble','weights','accuracy_mean','accuracy_std']].sort_values(by = ['weights', 'n_ensemble']).T
+    res = data_n[['n_ensemble','weights','accuracy_mean','accuracy_std']].sort_values(by = ['weights', 'n_ensemble'])#.T
     # To save the results
     os.chdir(path_to_save)
     nombre_csv = 'ResAccuracy_Bagging' + name
@@ -180,7 +244,7 @@ path_csv = os.chdir(root_path+'/Bagging_results')
 # Extraemos los nombres de todos los ficheros
 total_name_list = []
 for filename in os.listdir(path_csv):
-    if (filename.endswith('.csv') and 'Aggregated' in filename and 'combo' in filename and 'classes' not in filename):
+    if (filename.endswith('.csv') and 'Aggregated' in filename and 'combo' in filename and 'classes' not in filename and 'averaged' not in filename):
         total_name_list.append(filename)
 
 
@@ -190,6 +254,23 @@ for file in total_name_list:
     name = file[25:32]
     data = pd.read_csv(file)
     plot_acc_ensemble(data, name)
+
+
+path_to_save = root_path+'/Analysis_results'
+for file in total_name_list:
+    os.chdir(root_path + '/Bagging_results')
+    print(file)
+    name = file[25:]
+    data = pd.read_csv(file)
+
+    data_n = data[data['n_ensemble'].isin([15,50,100,150,199])]
+    res = data_n[['n_ensemble','weights','accuracy_mean','accuracy_std']].sort_values(by = ['weights', 'n_ensemble'])#.T
+    # To save the results
+    os.chdir(path_to_save)
+    nombre_csv = 'ResAccuracy_Bagging' + name
+    res.to_csv(nombre_csv, encoding='utf_8_sig',index=True)
+
+
 
 
 
@@ -252,6 +333,24 @@ for file in total_name_list:
     plot_acc_ensemble_averaged(data, name)
 
 
+
+#######################################################################
+#################    More weight in combo (hard, easy) instances WITH RANKING   #################
+#######################################################################
+path_csv = os.chdir(root_path+'/Bagging_results')
+# Extraemos los nombres de todos los ficheros
+total_name_list = []
+for filename in os.listdir(path_csv):
+    if (filename.endswith('.csv') and 'Aggregated' in filename and 'combo' in filename and 'averaged' in filename and 'classes' not in filename):
+        total_name_list.append(filename)
+
+
+for file in total_name_list:
+    os.chdir(root_path + '/Bagging_results')
+    print(file)
+    name = file[25:32]
+    data = pd.read_csv(file)
+    plot_acc_ensemble_averaged(data, name)
 
 
 
