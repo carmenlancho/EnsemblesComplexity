cf5d604e9ffb96429461cc310e48bc2e2eb7e803
carmenlancho <carmenlancho@hotmail.com>
2023-05-18 18:55:23 +0200

    small code changes

--- ComplexityDriven_Bagging.py
+++ ComplexityDriven_Bagging.py
@@ -997,7 +997,9 @@ for filename in os.listdir(path_csv):
 #     'page-blocks.csv']
 # total_name_list = ['ilpd.csv','diabetes.csv','segment.csv','sonar.csv']
 
-# total_name_list = ['ionosphere.csv','wdbc.csv', 'pima.csv']
+total_name_list = [#'breast-w.csv',
+                   'arrhythmia_cfs.csv', 'vertebral_column.csv',
+                   'diabetic_retinopathy.csv','banknote_authentication.csv']
 
 path_to_save = root_path+'/Bagging_results'
 n_ensembles = 200 # maximum number of ensembles to consider (later we plot and stop when we want)
