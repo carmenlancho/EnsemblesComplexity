204f8d1c56b865993ccb762808e252c0f26e5340
carmenlancho <carmenlancho@hotmail.com>
2024-01-29 20:29:27 +0100

    4 configurations of parameters

--- Comparison_Methods.py
+++ Comparison_Methods.py
@@ -21,6 +21,45 @@ df_total = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB.csv')
 # este archivo ha sido previamente agregado en el script Analysis_ConfigurationParameters
 
 
+
+#################################
+### COMPLEXITY-DRIVEN BAGGING: FILTRO DE PARÁMETROS ###
+#################################
+path_csv = os.chdir(root_path+'/Results_general_algorithm')
+df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB.csv')
+# este archivo ha sido previamente agregado en el script Analysis_ConfigurationParameters
+df_total_to_filter.shape
+
+# Aquí filtro los parámetros que no queremos
+# Comenzamos por alpha = [2,4,6,8,10] y s = [1,2,4,6,8,10,12,14,16,18,20] ## Filter1Params
+values_alpha = [12,14,16,18,20]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['alpha'].isin(values_alpha)]
+values_split = [22,24,26,28,30]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['split'].isin(values_split)]
+
+#  Seguimos con alpha = [2,4,6,8,10] y s = [1,2,4,6,8,10] ## Filter2Params
+values_alpha = [12,14,16,18,20]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['alpha'].isin(values_alpha)]
+values_split = [12,14,16,18,20,22,24,26,28,30]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['split'].isin(values_split)]
+
+# Seguimos con alpha = [2,4,6,8,10] y s = [1,2,4,8,12,16,20] ## Filter3Params
+values_alpha = [12,14,16,18,20]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['alpha'].isin(values_alpha)]
+values_split = [22,24,26,28,30,6,10,14,18]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['split'].isin(values_split)]
+
+# Seguimos con alpha = [2,6,10] y s = [1,2,4,8,14,20] ## Filter4Params
+values_alpha = [12,14,16,18,20,4,8]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['alpha'].isin(values_alpha)]
+values_split = [22,24,26,28,30,6,10,12,16,18]
+df_total_to_filter = df_total_to_filter[~df_total_to_filter['split'].isin(values_split)]
+
+
+
+
+
+
 #################################
 ###      STANDARD BAGGING     ###
 #################################
@@ -65,50 +104,55 @@ df_standard = pd.read_csv('SummarizeResults_StandardBagging.csv')
 ###       MIXED BAGGING       ###
 #################################
 
+# path_csv = os.chdir(root_path+'/MixedBagging/Adapted_results')
+#
+# # Extraemos los nombres de todos los ficheros
+# total_name_list = []
+# for filename in os.listdir(path_csv):
+#     if filename.endswith('.csv') and filename.startswith('Mixed_Bagging_aggregated'):
+#         total_name_list.append(filename)
+# # len(total_name_list)
+#
+# # General df to save all the results
+# cols = ['model','perf_measure','accuracy_mean_mean','accuracy_mean_median', 'accuracy_mean_std','Dataset']
+# df_mixed = pd.DataFrame(columns=cols)
+#
+# for data_file in total_name_list:
+#     print(data_file)
+#     file = data_file
+#     name_data = data_file[data_file.find('Mixed_Bagging_aggregated_') +
+#                           len('Mixed_Bagging_aggregated_'):data_file.rfind('.csv')]
+#     data = pd.read_csv(file)
+#     df_summary = data.groupby(by=['model','perf_measure'], as_index=False).agg({'perf_value_mean': [np.mean, np.median, np.std]})
+#     data.groupby(by=['perf_measure','model'], as_index=False).agg({'n_trees': 'median'})
+#     aa = data.loc[(data['model'] == 'AdaBoost') & (data['perf_measure'] == 'acc')]
+#     aa['perf_value_mean'].std()
+#
+#     df_summary.columns = ['model','perf_measure','accuracy_mean_mean','accuracy_mean_median',  'accuracy_mean_std']
+#     df_summary['Dataset'] = name_data
+#     # For now, we only keep Grouped_Mixed_Bagging, Incremental_Mixed_Bagging and accuracy
+#     models_v = ['Grouped_Mixed_Bagging', 'Incremental_Mixed_Bagging']
+#     perf_v = ['acc']
+#     df_summary = df_summary[df_summary['model'].isin(models_v) & df_summary['perf_measure'].isin(perf_v)]
+#
+#     df_mixed = pd.concat([df_mixed,df_summary])
+#
+# # To save the results
+# path_to_save = os.chdir(root_path+'/MixedBagging/Adapted_results')
+# os.chdir(path_to_save)
+# nombre_csv_agg = 'SummarizeResults_MixedBagging.csv'
+# df_mixed.to_csv(nombre_csv_agg, encoding='utf_8_sig', index=False)
+
 path_csv = os.chdir(root_path+'/MixedBagging/Adapted_results')
-
-# Extraemos los nombres de todos los ficheros
-total_name_list = []
-for filename in os.listdir(path_csv):
-    if filename.endswith('.csv') and filename.startswith('Mixed_Bagging_aggregated'):
-        total_name_list.append(filename)
-# len(total_name_list)
-
-# General df to save all the results
-cols = ['model','perf_measure','accuracy_mean_mean','accuracy_mean_median', 'accuracy_mean_std','Dataset']
-df_mixed = pd.DataFrame(columns=cols)
-
-for data_file in total_name_list:
-    print(data_file)
-    file = data_file
-    name_data = data_file[data_file.find('Mixed_Bagging_aggregated_') +
-                          len('Mixed_Bagging_aggregated_'):data_file.rfind('.csv')]
-    data = pd.read_csv(file)
-    df_summary = data.groupby(by=['model','perf_measure'], as_index=False).agg({'perf_value_mean': [np.mean, np.median, np.std]})
-    data.groupby(by=['perf_measure','model'], as_index=False).agg({'n_trees': 'median'})
-    aa = data.loc[(data['model'] == 'AdaBoost') & (data['perf_measure'] == 'acc')]
-    aa['perf_value_mean'].std()
-
-    df_summary.columns = ['model','perf_measure','accuracy_mean_mean','accuracy_mean_median',  'accuracy_mean_std']
-    df_summary['Dataset'] = name_data
-    # For now, we only keep Grouped_Mixed_Bagging, Incremental_Mixed_Bagging and accuracy
-    models_v = ['Grouped_Mixed_Bagging', 'Incremental_Mixed_Bagging']
-    perf_v = ['acc']
-    df_summary = df_summary[df_summary['model'].isin(models_v) & df_summary['perf_measure'].isin(perf_v)]
-
-    df_mixed = pd.concat([df_mixed,df_summary])
-
-# To save the results
-path_to_save = os.chdir(root_path+'/MixedBagging/Adapted_results')
-os.chdir(path_to_save)
-nombre_csv_agg = 'SummarizeResults_MixedBagging.csv'
-df_mixed.to_csv(nombre_csv_agg, encoding='utf_8_sig', index=False)
+df_mixed = pd.read_csv('SummarizeResults_MixedBagging.csv')
 
 
 
 #########################################################################################################
 #####                                SUMMARY TABLE OF ALL MODELS                                    #####
 #########################################################################################################
+# por el filtro
+df_total = df_total_to_filter
 
 table_comparison = pd.DataFrame(columns=['Dataset','Standard_Bag'])
 table_comparison['Dataset'] = df_standard['Dataset']
@@ -196,7 +240,7 @@ table_comparison.rename(columns = {'Table':'CDB_Best_F1'}, inplace = True)
 # path_to_save = os.chdir(root_path+'/Results_Comparison_Methods')
 # os.chdir(path_to_save)
 os.chdir(root_path+'/Results_Comparison_Methods')
-nombre_csv_agg = 'table_comparison.csv'
+nombre_csv_agg = 'table_comparison_Filter4Params.csv'
 table_comparison.to_csv(nombre_csv_agg, encoding='utf_8_sig', index=True)
 table_comparison
 
@@ -204,7 +248,7 @@ table_comparison
 best_param['param'] = ('a=' + best_param["alpha"].astype('str') +
                        ', s=' + best_param["split"].astype('str'))
 parameters_CM = best_param.pivot(index='Dataset', columns='weights', values='param')
-nombre_csv_agg_param = 'parameters_CM.csv'
+nombre_csv_agg_param = 'parameters_CM_Filter4Params.csv'
 parameters_CM.to_csv(nombre_csv_agg_param, encoding='utf_8_sig', index=True)
 
 
