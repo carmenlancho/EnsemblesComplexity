1d786961f56f2143b0963244a24476a11d485eea
carmenlancho <carmenlancho@hotmail.com>
2024-02-10 19:55:36 +0100

    Plots evolution accuracy

--- Comparison_Methods.py
+++ Comparison_Methods.py
@@ -27,8 +27,8 @@ df_total = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB.csv')
 #################################
 path_csv = os.chdir(root_path+'/Results_general_algorithm')
 # df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB.csv')
-# df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB_from20ensembles.csv')
-df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB_from30ensembles.csv')
+df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB_from20ensembles.csv')
+# df_total_to_filter = pd.read_csv('SummarizeResults_ParameterConfiguration_CDB_from30ensembles.csv')
 # este archivo ha sido previamente agregado en el script Analysis_ConfigurationParameters
 df_total_to_filter.shape
 
@@ -127,8 +127,8 @@ df_total_to_filter = df_total_to_filter[~df_total_to_filter['split'].isin(values
 
 path_csv = os.chdir(root_path+'/Results_StandardBagging')
 # df_standard = pd.read_csv('SummarizeResults_StandardBagging.csv') ## all n_ensembles
-# df_standard = pd.read_csv('SummarizeResults_StandardBagging_from20ensembles.csv') # from20ensembles
-df_standard = pd.read_csv('SummarizeResults_StandardBagging_from30ensembles.csv') # from 30 ensembles
+df_standard = pd.read_csv('SummarizeResults_StandardBagging_from20ensembles.csv') # from20ensembles
+# df_standard = pd.read_csv('SummarizeResults_StandardBagging_from30ensembles.csv') # from 30 ensembles
 
 
 ################################
@@ -187,8 +187,8 @@ df_standard = pd.read_csv('SummarizeResults_StandardBagging_from30ensembles.csv'
 
 path_csv = os.chdir(root_path+'/MixedBagging/Adapted_results')
 # df_mixed = pd.read_csv('SummarizeResults_MixedBagging.csv') # all n_trees
-# df_mixed = pd.read_csv('SummarizeResults_MixedBagging_from20ensembles.csv') # from 20 n_trees
-df_mixed = pd.read_csv('SummarizeResults_MixedBagging_from30ensembles.csv') # from 30 n_trees
+df_mixed = pd.read_csv('SummarizeResults_MixedBagging_from20ensembles.csv') # from 20 n_trees
+# df_mixed = pd.read_csv('SummarizeResults_MixedBagging_from30ensembles.csv') # from 30 n_trees
 
 
 
@@ -294,6 +294,9 @@ best_param['param'] = ('a=' + best_param["alpha"].astype('str') +
 parameters_CM = best_param.pivot(index='Dataset', columns='weights', values='param')
 nombre_csv_agg_param = 'parameters_CM_from30_Filter7Params.csv'
 parameters_CM.to_csv(nombre_csv_agg_param, encoding='utf_8_sig', index=True)
+nombre_csv_agg_param2 = 'best_param_from20_Filter6Params.csv'
+best_param.to_csv(nombre_csv_agg_param2, encoding='utf_8_sig', index=True)
+
 
 
  # FALTA ORDENAR POR COMPLEXITY
@@ -303,3 +306,169 @@ parameters_CM.to_csv(nombre_csv_agg_param, encoding='utf_8_sig', index=True)
 #########################################################################################################
 #####                                  PLOT OF THE EVOLUTION                                        #####
 #########################################################################################################
+
+###### Read and plot standard bagging ####################################################
+path_csv = os.chdir(root_path+'/Results_StandardBagging')
+
+# Extraemos los nombres de todos los ficheros
+total_name_list = []
+for filename in os.listdir(path_csv):
+    if filename.endswith('.csv') and filename.startswith('Aggregated'):
+        total_name_list.append(filename)
+# len(total_name_list)
+
+
+# General df to save all the results
+cols = ['n_ensemble','StandardBagging_accuracy','Dataset']
+df_to_plot_standard = pd.DataFrame(columns=cols)
+for data_file in total_name_list:
+    # print(data_file)
+    file = data_file
+    name_data = data_file[data_file.find('AggregatedResults_StandardBagging_') +
+                          len('AggregatedResults_StandardBagging_'):data_file.rfind('.csv')]
+    data_df = pd.read_csv(file)
+    data_df2 = data_df.iloc[:,[0,2]].copy()
+    data_df2['Dataset'] = name_data
+    data_df2.columns = cols
+    df_to_plot_standard = pd.concat([df_to_plot_standard, data_df2])
+
+# df_to_plot_standard['n_ensemble'] = df_to_plot_standard['n_ensemble'] + 1
+# df_to_plot_standard[df_to_plot_standard['n_ensemble'] ==1] = 0
+
+# # Plot
+# dataset = 'profb'
+# data_plot = df_to_plot.loc[(df_to_plot['Dataset'] == dataset) & (df_to_plot['n_ensemble']>10)]
+# plt.plot(data_plot['n_ensemble'],data_plot['StandardBagging_accuracy'],'o', linestyle = 'dotted',label = 'StandardBagging')
+# plt.show()
+
+###### Read and plot Mixed and incremental bagging ####################################################
+
+
+path_csv = os.chdir(root_path+'/MixedBagging/Adapted_results')
+
+# Extraemos los nombres de todos los ficheros
+total_name_list = []
+for filename in os.listdir(path_csv):
+    if filename.endswith('.csv') and filename.startswith('Mixed_Bagging_aggregated'):
+        total_name_list.append(filename)
+
+cols = ['n_ensemble','GroupedBagging_accuracy','Dataset','IncrementalBagging_accuracy']
+df_to_plot_mixed = pd.DataFrame(columns=cols)
+
+for data_file in total_name_list:
+    print(data_file)
+    file = data_file
+    name_data = data_file[data_file.find('Mixed_Bagging_aggregated_') +
+                          len('Mixed_Bagging_aggregated_'):data_file.rfind('.csv')]
+    data_df = pd.read_csv(file)
+    data_df2 = data_df.loc[(data_df['model']=='Grouped_Mixed_Bagging') & (data_df['perf_measure']=='acc'), ['n_trees','perf_value_mean']]
+    data_df2['Dataset'] = name_data
+    data_df2.reset_index(drop=True, inplace=True)
+    var = data_df.loc[
+        (data_df['model'] == 'Incremental_Mixed_Bagging') & (data_df['perf_measure'] == 'acc'), ['perf_value_mean']]
+    var.reset_index(drop=True, inplace=True)
+    data_df2['Incremental_Mixed_Bagging'] = var
+    data_df2.columns = cols
+    df_to_plot_mixed = pd.concat([df_to_plot_mixed, data_df2])
+
+
+# Plot
+dataset = 'profb'
+data_plot = df_to_plot_mixed.loc[(df_to_plot_mixed['Dataset'] == dataset) & (df_to_plot_mixed['n_ensemble']>10)]
+plt.plot(data_plot['n_ensemble'],data_plot['GroupedBagging_accuracy'],'o', linestyle = 'dotted',
+         label = 'Grouped_Mixed_Bagging',color = 'k')
+plt.plot(data_plot['n_ensemble'],data_plot['IncrementalBagging_accuracy'],'o', linestyle = 'dashed',
+         label = 'Incremental_Mixed_Bagging',color='orange')
+plt.legend()
+plt.show()
+
+
+
+
+
+list_datasets = ['WineQualityRed_5vs6', 'Yeast_CYTvsNUC',
+       'arrhythmia_cfs', 'bands', 'banknote_authentication', 'bodyfat',
+       'breast-w', 'breastcancer', 'bupa', 'chatfield_4',
+       'contraceptive_LS', 'contraceptive_NL', 'contraceptive_NS',
+       'credit-g', 'diabetes', 'diabetic_retinopathy', 'fri_c0_250_50',
+       'glass0', 'glass1', 'hill_valley_without_noise_traintest', 'ilpd',
+       'ionosphere', 'liver-disorders', 'mammographic', 'musk1',
+       'parkinsons', 'phoneme', 'pima', 'profb', 'ring', 'saheart',
+        'sonar', 'spambase', 'spectfheart',
+       'steel-plates-fault', 'sylvine', 'teaching_assistant_LH',
+       'teaching_assistant_LM', 'teaching_assistant_MH', 'titanic',
+       'vehicle2', 'vertebral_column', 'wdbc', 'yeast1']
+len(list_datasets)
+
+############## Complexity driven Bagging
+
+path_csv = os.chdir(root_path+'/Results_general_algorithm/Aggregated_results')
+# Extraemos los nombres de todos los ficheros
+total_name_list = []
+for filename in os.listdir(path_csv):
+    if filename.endswith('.csv') and filename.startswith('Aggregated'):
+        total_name_list.append(filename)
+
+path_csv = os.chdir(root_path+'/Results_Comparison_Methods')
+
+best_param = pd.read_csv('best_param_from20_Filter6Params.csv')
+best_param_host = best_param.loc[best_param['weights'] == 'Hostility',['Dataset','alpha','split']]
+
+path_csv = os.chdir(root_path+'/Results_general_algorithm/Aggregated_results')
+# General df to save all the results
+cols = ['n_ensemble','CDB_Host_accuracy','Dataset']
+df_to_plot_cdb = pd.DataFrame(columns=cols)
+for dataset_i in list_datasets:
+    print(dataset_i)
+    # file = data_file
+    alpha = best_param_host.loc[best_param_host['Dataset']==dataset_i,['alpha']].values[0]
+    split = best_param_host.loc[best_param_host['Dataset'] == dataset_i, ['split']].values[0]
+    param_to_search = dataset_i+ '_split'+str(split[0]) +'_alpha'+str(alpha[0])
+
+    matching_file = [s for s in total_name_list if param_to_search in s][0]
+
+    data_df = pd.read_csv(matching_file)
+    data_df2 = data_df.loc[data_df['weights'] == 'Hostility', ['n_ensemble', 'accuracy_mean']].copy()
+    data_df2['Dataset'] = dataset_i
+    data_df2.columns = cols
+    df_to_plot_cdb = pd.concat([df_to_plot_cdb, data_df2])
+
+
+df_to_plot_cdb.reset_index(drop=True,inplace=True)
+
+
+#### Plot
+
+def plot_acc_ensembles(dataset_name, path_to_save):
+
+    data_plot_standard = df_to_plot_standard.loc[(df_to_plot_standard['Dataset'] == dataset_name) & (df_to_plot_standard['n_ensemble']>10)]
+    data_plot_cbd = df_to_plot_cdb.loc[(df_to_plot_cdb['Dataset'] == dataset_name) & (df_to_plot_cdb['n_ensemble']>10)]
+    df_plot_mixed = df_to_plot_mixed.loc[(df_to_plot_mixed['Dataset'] == dataset_name) & (df_to_plot_mixed['n_ensemble']>10)]
+
+    plt.plot(data_plot_standard['n_ensemble'],data_plot_standard['StandardBagging_accuracy'],'P', linestyle = 'dotted',
+             label = 'Standard Bagging',color = 'k')
+    plt.plot(data_plot_cbd['n_ensemble'],data_plot_cbd['CDB_Host_accuracy'],'o', linestyle = 'dashed',
+             label = 'CDB_Hostility',color='orange')
+    plt.plot(df_plot_mixed['n_ensemble'],df_plot_mixed['GroupedBagging_accuracy'],'^', linestyle = 'dotted',
+             label = 'Grouped_Mixed_Bagging',color = 'c')
+    plt.plot(df_plot_mixed['n_ensemble'],df_plot_mixed['IncrementalBagging_accuracy'],'s', linestyle = 'dashed',
+             label = 'Incremental_Mixed_Bagging',color='red')
+    plt.legend()
+    plt.title(dataset_name)
+    plt.tight_layout()
+    # plt.show()
+    os.chdir(path_to_save)
+    plt.savefig('Accuracy_evolution_From20Ensembles_Host_' + str(dataset_name) + '.png')
+    plt.clf()
+
+    return
+
+
+# dataset_name = 'profb'
+path_to_save = root_path+'/Results_Comparison_Methods'
+
+for dataset_name in list_datasets:
+    plot_acc_ensembles(dataset_name, path_to_save)
+
+
+
