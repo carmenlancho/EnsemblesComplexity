2cf4cd6badf5f0e8624d56a6973aec8d2c9e4d9b
carmenlancho <carmenlancho@hotmail.com>
2024-07-25 18:15:21 +0200

    CV boosting function with confusion matrix

--- Boosting_algorithm.py
+++ Boosting_algorithm.py
@@ -43,90 +43,90 @@ y[y==0] = -1 # sign format
 
 M = 20 # number of models, ensemble size
 
-
-skf = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-fold = 0
-for train_index, test_index in skf.split(X, y):
-    fold = fold + 1
-    print(fold)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    n = len(y_train)
-    n_test = len(y_test)
-
-    # List to save classifiers
-    clf_list = []
-    # List to save alphas
-    alpha_list = []
-
-    # Preds_train
-    preds_train = []
-    # Preds_test
-    preds_test = []
-    # Exponential loss
-    exp_loss_avg = []
-    # Misclassification rate
-    misc_rate = []
-
-
-    # Initialize weights
-    if (method_weights == 'classic'):
-        weights_v = np.ones(n) / n
-    else: # algo de complejidad
-        weights_v = np.ones(n) / n # sera una función de la complejidad
-
-
-    for m in range(M):
-        print(m)
-        # m = 0
-
-        # Fit a classifier with the specific weights
-        clf_m = DecisionTreeClassifier(random_state=0,max_depth=1)
-        clf_m.fit(X_train, y_train, sample_weight=weights_v)
-        # We append the classifier to the list
-        clf_list.append(clf_m)
-
-
-        # Prediction and calculation of error
-        y_pred = clf_m.predict(X_train)
-        preds_train.append(y_pred)
-        disagree = np.not_equal(y_train, y_pred)
-        error_m = (sum(weights_v * disagree)) / sum(weights_v)
-
-
-        # Compute alpha_m
-        alpha_m = np.log((1-error_m)/error_m)
-        alpha_list.append(alpha_m)
-
-        # Evaluate on test
-        y_pred_test = clf_m.predict(X_test)
-        preds_test.append(y_pred_test)
-
-        # Update the observations weights
-        weights_v[disagree] = weights_v[disagree] * np.exp(alpha_m)
-
-
-    # Predictions for train and test
-    df_preds_train = np.zeros(n)
-    df_preds_test = np.zeros(n_test)
-    for m in range(M):
-        df_preds_train = df_preds_train + (preds_train[m] * alpha_list[m])
-        exp_loss_avg_m = (1/n)*np.sum(np.exp(-np.sign(df_preds_train)*y_train))
-        exp_loss_avg.append(exp_loss_avg_m)
-        misc_rate_m = np.sum(np.not_equal(y_train, np.sign(df_preds_train)))/n
-        misc_rate.append(misc_rate_m)
-        df_preds_test = df_preds_test + (preds_test[m] * alpha_list[m])
-
-    final_pred_train = np.sign(df_preds_train)
-    final_pred_test = np.sign(df_preds_test)
-
-    # Plot misclassification rate and exponential loss
-    iterations = np.arange(1,M+1)
-    plt.plot(iterations, exp_loss_avg, label="Average exponential loss",color='#1AB7D3')
-    plt.plot(iterations, misc_rate, label="Misclassification rate", color='crimson')
-    plt.xlabel('Number of boosting iterations')
-    plt.legend()
-    plt.show()
+#
+# skf = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+# fold = 0
+# for train_index, test_index in skf.split(X, y):
+#     fold = fold + 1
+#     print(fold)
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+#     n = len(y_train)
+#     n_test = len(y_test)
+#
+#     # List to save classifiers
+#     clf_list = []
+#     # List to save alphas
+#     alpha_list = []
+#
+#     # Preds_train
+#     preds_train = []
+#     # Preds_test
+#     preds_test = []
+#     # Exponential loss
+#     exp_loss_avg = []
+#     # Misclassification rate
+#     misc_rate = []
+#
+#
+#     # Initialize weights
+#     if (method_weights == 'classic'):
+#         weights_v = np.ones(n) / n
+#     else: # algo de complejidad
+#         weights_v = np.ones(n) / n # sera una función de la complejidad
+#
+#
+#     for m in range(M):
+#         print(m)
+#         # m = 0
+#
+#         # Fit a classifier with the specific weights
+#         clf_m = DecisionTreeClassifier(random_state=0,max_depth=1)
+#         clf_m.fit(X_train, y_train, sample_weight=weights_v)
+#         # We append the classifier to the list
+#         clf_list.append(clf_m)
+#
+#
+#         # Prediction and calculation of error
+#         y_pred = clf_m.predict(X_train)
+#         preds_train.append(y_pred)
+#         disagree = np.not_equal(y_train, y_pred)
+#         error_m = (sum(weights_v * disagree)) / sum(weights_v)
+#
+#
+#         # Compute alpha_m
+#         alpha_m = np.log((1-error_m)/error_m)
+#         alpha_list.append(alpha_m)
+#
+#         # Evaluate on test
+#         y_pred_test = clf_m.predict(X_test)
+#         preds_test.append(y_pred_test)
+#
+#         # Update the observations weights
+#         weights_v[disagree] = weights_v[disagree] * np.exp(alpha_m)
+#
+#
+#     # Predictions for train and test
+#     df_preds_train = np.zeros(n)
+#     df_preds_test = np.zeros(n_test)
+#     for m in range(M):
+#         df_preds_train = df_preds_train + (preds_train[m] * alpha_list[m])
+#         exp_loss_avg_m = (1/n)*np.sum(np.exp(-np.sign(df_preds_train)*y_train))
+#         exp_loss_avg.append(exp_loss_avg_m)
+#         misc_rate_m = np.sum(np.not_equal(y_train, np.sign(df_preds_train)))/n
+#         misc_rate.append(misc_rate_m)
+#         df_preds_test = df_preds_test + (preds_test[m] * alpha_list[m])
+#
+#     final_pred_train = np.sign(df_preds_train)
+#     final_pred_test = np.sign(df_preds_test)
+#
+#     # Plot misclassification rate and exponential loss
+#     iterations = np.arange(1,M+1)
+#     plt.plot(iterations, exp_loss_avg, label="Average exponential loss",color='#1AB7D3')
+#     plt.plot(iterations, misc_rate, label="Misclassification rate", color='crimson')
+#     plt.xlabel('Number of boosting iterations')
+#     plt.legend()
+#     plt.show()
 
 
 ## Función general
@@ -156,6 +156,9 @@ def boosting_algorithm(X_train,y_train,X_test,y_test,M,method_weights, plot_erro
     exp_loss_avg = []
     # Misclassification rate
     misc_rate = []
+    misc_rate_test = []
+    # confusion matrix
+    conf_matrix = []
 
     # Initialize weights
     if (method_weights == 'classic'):
@@ -200,6 +203,11 @@ def boosting_algorithm(X_train,y_train,X_test,y_test,M,method_weights, plot_erro
         misc_rate_m = np.sum(np.not_equal(y_train, np.sign(df_preds_train))) / n_train
         misc_rate.append(misc_rate_m)
         df_preds_test = df_preds_test + (preds_test[m] * alpha_list[m])
+        misc_rate_test_m = np.sum(np.not_equal(y_test, np.sign(df_preds_test))) / n_test
+        misc_rate_test.append(misc_rate_test_m)
+        final_pred_test_m = np.sign(df_preds_test)
+        conf_matrix.append(confusion_matrix(y_test, final_pred_test_m).tolist())
+
 
     final_pred_train = np.sign(df_preds_train)
     final_pred_test = np.sign(df_preds_test)
@@ -213,12 +221,12 @@ def boosting_algorithm(X_train,y_train,X_test,y_test,M,method_weights, plot_erro
         plt.legend()
         plt.show()
 
-    return final_pred_train, final_pred_test, exp_loss_avg, misc_rate
+    return final_pred_train, final_pred_test, exp_loss_avg, misc_rate, misc_rate_test, conf_matrix
 
-method_weights = 'classic'
-plot_error = False
-M = 20  # number of models, ensemble size
-final_pred_train, final_pred_test, exp_loss_avg, misc_rate =  boosting_algorithm(X_train,y_train,X_test,y_test,M,method_weights, plot_error)
+# method_weights = 'classic'
+# plot_error = False
+# M = 20  # number of models, ensemble size
+# final_pred_train, final_pred_test, exp_loss_avg, misc_rate, misc_rate_test, conf_matrix =  boosting_algorithm(X_train,y_train,X_test,y_test,M,method_weights, plot_error)
 
 
 ### Cross-Validation Boosting
@@ -230,10 +238,11 @@ def CV_boosting(dataset,X,y,M,method_weights, plot_error,n_cv_splits):
 
     dataset_v = [dataset for _ in range(M)]
     n_ensemble_v = list(np.arange(1,M+1))
-    weights_v = [method_weights] * M
+    weights_type = [method_weights] * M
 
     # dataframe to save the results
-    results = pd.DataFrame(columns=['dataset','fold','n_ensemble','weights','exp_loss_avg','misc_rate'])
+    results = pd.DataFrame(columns=['dataset','fold','n_ensemble','method_weights','exp_loss_avg_train',
+                                    'misc_rate_train','misc_rate_test','conf_matrix_test'])
 
 
     skf = StratifiedKFold(n_splits=n_cv_splits, random_state=1, shuffle=True)
@@ -244,13 +253,15 @@ def CV_boosting(dataset,X,y,M,method_weights, plot_error,n_cv_splits):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        final_pred_train, final_pred_test, exp_loss_avg, misc_rate = boosting_algorithm(X_train, y_train, X_test,
+        final_pred_train, final_pred_test, exp_loss_avg, misc_rate, misc_rate_test,conf_matrix = boosting_algorithm(X_train, y_train, X_test,
                                                                                         y_test, M, method_weights,
                                                                                         plot_error)
 
         fold_v = [fold]*M
-        results_dict = {'dataset':dataset_v,'fold':fold_v,'n_ensemble':n_ensemble_v,'weights':weights_v,
-                        'exp_loss_avg':exp_loss_avg,'misc_rate':misc_rate} # falta por añadir confusion matrix
+        results_dict = {'dataset':dataset_v,'fold':fold_v,'n_ensemble':n_ensemble_v,'method_weights':weights_type,
+                        'exp_loss_avg_train':exp_loss_avg,'misc_rate_train':misc_rate,
+                        'misc_rate_test':misc_rate_test,'conf_matrix_test':conf_matrix} # falta por añadir confusion matrix
+
         results_aux = pd.DataFrame(results_dict)
         results = pd.concat([results, results_aux])
         results.reset_index(drop=True, inplace=True)
